955a4abedf50ccbe03268e2727f8bc22bffbfa31
Mashno <118561914+Mashno@users.noreply.github.com>
2025-02-24 16:17:39 +0300

    2_commit
    добавил класс Heretic

---
+++ MavenProject/src/main/java/com/mycompany/mavenproject/Heretic.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject;
+
+/**
+ *
+ * @author Владислав
+ */
+public class Heretic {
+    private String name;
+    private String crimes;
+    private String last_appear;
+    private int id;
+    private String level_threat;
+    
+    public Heretic(String name, String crimes, String last_appear, int id, String level_threat){
+        this.name = name;
+        this.crimes = crimes;
+        this.last_appear = last_appear;
+        this.id = id;
+        this.level_threat = level_threat;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCrimes(String crimes) {
+        this.crimes = crimes;
+    }
+
+    public void setLast_appear(String last_appear) {
+        this.last_appear = last_appear;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLevel_threat(String level_threat) {
+        this.level_threat = level_threat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCrimes() {
+        return crimes;
+    }
+
+    public String getLast_appear() {
+        return last_appear;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLevel_threat() {
+        return level_threat;
+    }
+    
+    
+}
