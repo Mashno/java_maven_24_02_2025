29dba0f013f23ad184881b9349797b070827fe69
Mashno <118561914+Mashno@users.noreply.github.com>
2025-02-24 16:05:41 +0300

    1_commit
    создание ветки

--- MavenProject/src/main/java/com/mycompany/mavenproject/MavenProject.java
+++ MavenProject/src/main/java/com/mycompany/mavenproject/MavenProject.java
@@ -14,14 +14,6 @@ import com.github.javafaker.Faker;
 public class MavenProject {
 
     public static void main(String[] args) {
-        Faker faker = new Faker();
-
-        String name = faker.name().fullName();
-        String address = faker.address().fullAddress();
-        String email = faker.internet().emailAddress();
-
-        System.out.println("Фейковое имя: " + name);
-        System.out.println("Фейковый адрес: " + address);
-        System.out.println("Фейковый email: " + email);
+     
     }
 }
